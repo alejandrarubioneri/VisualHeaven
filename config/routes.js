786a9eb724cf5439d9b5e4eb5d3cdb72a95d2f9f
Alejandra Rubio <alejandrarubio@MacBook-Air-de-Alberto-2.local>
2021-07-12 20:41:24 +0200

    esqueleto hecho

---
+++ config/routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+
+const miscController = require('../controllers/misc.controller');
+
+router.get('/', miscController.home);
+
+module.exports = router;
