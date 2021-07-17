61c699ab8cc43eff9c377760884c654fd9bd9061
Paloma Borro <palomabs93@gmail.com>
2021-07-17 13:57:23 +0200

    Añadido el middleware de autenticación y modificado el perfil de usuario

--- bin/seeds.js
+++ bin/seeds.js
@@ -3,8 +3,9 @@ require("../config/db.config.js");
 const User = require('../models/User.model');
 const Offer = require('../models/Offer.model');
 
-const faker = require("faker");
+const faker = require('faker');
 const mongoose = require('mongoose');
+const toonavatar = require('cartoon-avatar');
 
 
 mongoose.connection.once('connected', () => {
@@ -17,77 +18,77 @@ mongoose.connection.once('connected', () => {
                 fullName: 'Leona',
                 email: 'testing1@gmail.com',
                 password: '12345678',
-                image: faker.internet.avatar(),
+                image: toonavatar.generate_avatar({"gender":"female"}),
             }, {
                 fullName: 'Ahri',
                 email: 'testing2@gmail.com',
                 password: '12345678',
-                image: faker.internet.avatar(),
+                image: toonavatar.generate_avatar({"gender":"female"}),
             }, {
                 fullName: 'Shen',
                 email: 'testing3@gmail.com',
                 password: '12345678',
-                image: faker.internet.avatar(),
+                image: toonavatar.generate_avatar({"gender":"male"}),
             }, {
                 fullName: 'Akali',
                 email: 'testing4@gmail.com',
                 password: '12345678',
-                image: faker.internet.avatar(),
+                image: toonavatar.generate_avatar({"gender":"female"}),
             }, {
                 fullName: 'Taliyah',
                 email: 'testing5@gmail.com',
                 password: '12345678',
-                image: faker.internet.avatar(),
+                image: toonavatar.generate_avatar({"gender":"female"}),
             }, {
                 fullName: 'Taric',
                 email: 'testing6@gmail.com',
                 password: '12345678',
-                image: faker.internet.avatar(),
+                image: toonavatar.generate_avatar({"gender":"male"}),
             }, {
                 fullName: 'Vayne',
                 email: 'testing7@gmail.com',
                 password: '12345678',
-                image: faker.internet.avatar(),
+                image: toonavatar.generate_avatar({"gender":"female"}),
             }, {
                 fullName: 'Zoe',
                 email: 'testing8@gmail.com',
                 password: '12345678',
-                image: faker.internet.avatar(),
+                image: toonavatar.generate_avatar({"gender":"female"}),
             }, {
                 fullName: 'Pyke',
                 email: 'testing9@gmail.com',
                 password: '12345678',
-                image: faker.internet.avatar(),
+                image: toonavatar.generate_avatar({"gender":"male"}),
             }, {
                 fullName: 'Jinx',
                 email: 'testing10@gmail.com',
                 password: '12345678',
-                image: faker.internet.avatar(),
+                image: toonavatar.generate_avatar({"gender":"female"}),
             }, {
                 fullName: 'Bard',
                 email: 'testing11@gmail.com',
                 password: '12345678',
-                image: faker.internet.avatar(),
+                image: toonavatar.generate_avatar({"gender":"male"}),
             }, {
                 fullName: 'Maokai',
                 email: 'testing12@gmail.com',
                 password: '12345678',
-                image: faker.internet.avatar(),
+                image: toonavatar.generate_avatar({"gender":"male"}),
             }, {
                 fullName: 'Lucian',
                 email: 'testing13@gmail.com',
                 password: '12345678',
-                image: faker.internet.avatar(),
+                image: toonavatar.generate_avatar({"gender":"male"}),
             }, {
                 fullName: 'Lux',
                 email: 'testing14@gmail.com',
                 password: '12345678',
-                image: faker.internet.avatar(),
+                image: toonavatar.generate_avatar({"gender":"female"}),
             }, {
                 fullName: 'Kayle',
                 email: 'testing15@gmail.com',
                 password: '12345678',
-                image: faker.internet.avatar(),
+                image: toonavatar.generate_avatar({"gender":"female"}),
             }]
             return User.create(usersToCreate)
         })
---
+++ controllers/users.controller.js
@@ -0,0 +1,10 @@
+const User = require('../models/User.model');
+const Offer = require('../models/Offer.model');
+
+
+// Render del perfil
+module.exports.profile = (req,res, next) => {
+    Offer.find({ author: req.user._id })
+
+    res.render('profile')
+}
