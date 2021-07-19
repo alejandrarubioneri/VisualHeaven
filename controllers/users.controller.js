e2d2a53c77a0e305d91be6e000bcbe093d9de731
Paloma Borro <palomabs93@gmail.com>
2021-07-19 18:58:39 +0200

    cambios en vistas

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,39 +3,10 @@ const Offer = require('../models/Offer.model');
 
 
 // Render del perfil
-// module.exports.profile = (req,res, next) => {
-//     Offer.find({ author: req.user._id })
-//     .then(offers => {
-//         res.render('profile', { offers: offers })
-//     })
-// }
+module.exports.profile = (req,res, next) => {
+    Offer.find({ author: req.user._id })
+    .then(offers => {
+        res.render('profile', { offers: offers })
+    })
+}
 
-module.exports.profile = (req, res, next) => {
-    function findProjectsAndRender(user) {
-      Project.find({ owner: userId ? userId : req.user._id })
-        .populate("owner")
-        .then(projects => {
-          const data = {
-            projects: projects
-          }
-      
-          if (user) {
-            data.currentUser = user
-          }
-  
-          res.render('profile', data);
-        })
-        .catch(next)
-    }
-  
-    const { userId } = req.params;
-  
-    if (userId) {
-      User.findById(userId)
-        .then(user => {
-          findProjectsAndRender(user)
-        })
-    } else {
-      findProjectsAndRender()
-    }
-  }
