4bebbf6fbc9e0d135010b99abd1b90fba8d7195b
Paloma Borro <palomabs93@gmail.com>
2021-07-23 19:37:58 +0200

    formulario sin funcionar pero botón de aplicar funcional

--- controllers/offers.controller.js
+++ controllers/offers.controller.js
@@ -5,49 +5,67 @@ const Application = require('../models/Application.model');
 
 // Render de offers
 module.exports.offers = (req, res, next) => {
-      Offer.find()
-        .populate('author')
-        .then(offers => {
-          res.render('offers', {
-            offers: offers
-          });
-        })
-        .catch(next)
-  }   
+  Offer.find()
+    .populate('author')
+    .then(offers => {
+      res.render('offers', {
+        offers: offers
+      });
+    })
+    .catch(next)
+}
 
 // Formulario creación de oferta
 module.exports.doOffers = (req, res, next) => {
-  console.log(req)  
+  console.log(req)
   Offer.create(req.body)
-      .then((offer) => {
-        res.redirect('/offers');
-      })
-      .catch((e) => res.render('error'));
-  };
+    .then((offer) => {
+      res.redirect('/offers');
+    })
+    .catch((e) => res.render('error'));
+};
 
 
-  // Render del detalle de las ofertas con botón de apply
-  module.exports.offersDetail = (req, res, next) => {
-    Offer.findById(req.params.id)
+// Render del detalle de las ofertas con botón de apply
+module.exports.offersDetail = (req, res, next) => {
+  Offer.findById(req.params.id)
     .populate('author')
     .then(offer => {
-      return Application.findOne({ user: req.user._id, offer: req.params.id})
-      .then(application => {
-        const applied = !!application; //Para convertir application a booleano
-        res.render('offerDetail', {
-          offer: offer, applied: applied
-        });
-      })
+      return Application.findOne({
+          user: req.user._id,
+          offer: req.params.id
+        })
+        .then(application => {
+          const applied = !!application; //Para convertir application a booleano
+          res.render('offerDetail', {
+            offer: offer,
+            applied: applied
+          });
+        })
     })
     .catch(next)
-  }
+}
 
-  // Aplicar a las ofertas
-  module.exports.apply = (req, res, next) => {
-    Application.findOneAndDelete({ user: req.user._id, offer: req.params.id}) // Saber si ese usuario ya ha aplicado para borrar el botón
-      .then(apply => {
-        if (apply) {
-          res.json({ deleted: !! application }) // Borrar el botón
-        }
-      })
-  }
+// Aplicar a las ofertas
+module.exports.apply = (req, res, next) => {
+  Application.findOneAndDelete({
+      user: req.user._id,
+      offer: req.params.id
+    }) // Saber si ese usuario ya ha aplicado para borrar el botón
+    .then(apply => {
+      if (!apply) {
+       return Application.create({
+            user: req.user._id,
+            offer: req.params.id
+          })
+          .then(() => res.json({
+            applied: false
+          }))
+      } else {
+        res.json({
+          applied: true // Borrar el botón
+        }) 
+      }
+    })
+    .catch(next)
+}
